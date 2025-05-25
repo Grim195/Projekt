b4e500b1f966c0a8f6ec5cc62a7d99dc51739832
Grim195 <alexplastiak95@gmail.com>
2025-05-25 14:22:44 +0200

    uprava stranky, pridanie filmov

--- moviesingle.php
+++ moviesingle.php
@@ -12,7 +12,7 @@ include_once "parts/header.php";
     <div class="container">
         <div class="row">
             <div class="col-md-12">
-                <h1><?= htmlspecialchars($movie['title']) ?></h1>
+                <h1 class="movie_title"><?= htmlspecialchars($movie['title']) ?></h1>
             </div>
         </div>
     </div>
